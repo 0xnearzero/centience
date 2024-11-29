2787c21edd9a981b4c00a8f05177cf51fe53f370
Orpheas <nearzeromemetic@gmail.com>
2024-11-29 15:58:38 +0200

    Update defaultCharacter.ts

--- packages/core/src/core/defaultCharacter.ts
+++ packages/core/src/core/defaultCharacter.ts
@@ -7,7 +7,6 @@ export const defaultCharacter: Character = {
     modelProvider: ModelProvider.LLAMACLOUD,
     settings: {
         secrets: {
-            "DISCORD_API_TOKEN": "MTMxMTgzODg0Mzc0NTYwMzY4NA.GSyLwo.CfjNfgHyX_J7O-uuWSoiwODRkdeWnNND73RyQ8"
         },
         voice: {
             model: "en_US-hfc_female-medium",
